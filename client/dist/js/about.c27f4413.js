1c7104833e059e94414e4d0827d78e36aa15ca87
riffca <riffca@yandex.ru>
2020-06-01 11:57:40 +0300

    new page

---
+++ client/dist/js/about.c27f4413.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{b3c3:function(t,e,s){"use strict";s.r(e);var n=function(){var t=this,e=t.$createElement,s=t._self._c||e;return s("div",{staticClass:"results page"},[s("tableList")],1)},a=[],c=s("e4bc"),o={name:"Home",components:{tableList:c["a"]},created:function(){this.$store.dispatch("forks/fetchForks",{})}},i=o,l=s("2877"),r=Object(l["a"])(i,n,a,!1,null,null,null);e["default"]=r.exports}}]);
+//# sourceMappingURL=about.c27f4413.js.map
